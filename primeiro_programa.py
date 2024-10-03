ba89b89120f9af4dc0d9399babb64b21874a66e5
Johnnatan Krause <johnnatankrause@gmail.com>
2024-10-03 20:46:17 -0300

    Adicionando novos arquivos e materiais de apoio

---
+++ primeiro_programa.py
@@ -0,0 +1,3 @@
+# Como realizar um print semplis!
+print("Oi, seja bem vindo ao curso de python!")
+print("python")
---
+++ variaveis_constantes.py
@@ -0,0 +1,20 @@
+#Variáveis e Constantes em PY
+#•	Não é preciso declara o tipo das variáveis em PY;
+#•	Não é preciso declarar os tipos das variáveis em PY;
+#•	Não existe palavras reservadas em PY como existem em JS;
+#•	Para se reservar uma variável em PY escrevemos todos os nomes em MAIUSCULO, ISSO É UMA CONVENÇÃO entre os programadores!!!;
+#•	O padrão de nomes deve ser snake case, ou seja, usa-se o _ para não deixar o espaço em branco;
+#•	Escolher nomes sugestivos;
+#•	Entre outras;
+
+#Comentários são feitos de duas formas:
+
+#•	# Você comenta uma linha do código;
+#•	“”” “”” Você comenta tudo que estiver dentro das aspas;
+
+nome = "Johnnatan Krause"
+idade = 36
+BRAZILIAN_STATES = ["SP", "RJ", "PA", "PR"]
+
+nome, idade = ("João, 40")
+print(nome, idade)
